d006807c4dab92f2d836044e93f79358a8f4c7ba
sharonmerlinnadar-ship-it <sharonmerlinnadar@gmail.com>
2026-02-08 01:28:48 +0530

    Delete script.js
    Signed-off-by: sharonmerlinnadar-ship-it <sharonmerlinnadar@gmail.com>

--- script.js
+++
@@ -1,7 +0,0 @@
-const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg"];
-let index = 0;
-
-setInterval(() => {
-  index = (index + 1) % images.length;
-  document.getElementById("slide").src = images[index];
-}, 2500);
